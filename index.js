e9786b8d55af0bd22914b90f04abfc3443d96ed5
Spiridonov Dmitry <sydd2005@gmail.com>
2019-03-18 01:13:35 +0300

    IngredientsList as ES6 class

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ var items = [
     "3 cloves of Garlic"
 ];
 
-const IngredientsList = React.createClass({
-    displayName: "IngredientsList",
+class IngredientsList extends React.Component {
     renderListItem(ingredient, i) {
         return React.createElement("li", {key: i}, ingredient);
-    },
+    }
+
     render() {
         return React.createElement("ul",
             {"className": "ingredients"},
             this.props.items.map(this.renderListItem)
         );
     }
-});
+}
 
 const list = React.createElement(IngredientsList, {items}, null);
 
