dc2c63c180beb30d5f37357f09114fc9eae42332
Spiridonov Dmitry <sydd2005@gmail.com>
2019-03-18 23:47:25 +0300

    moved index.html to dist and changed references to bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist/assets"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
   module: {
